569ef98dd014923e2d3251c3f21ae91a68ff5e81
JeremyRand <biolizard89@gmail.com>
2016-07-19 11:21:13 +0000

    Update to newer libdohj-namecoin API and webbtc API.

--- bitcoinj-server/src/main/java/org/namecoin/bitcoinj/spring/service/NameLookupService.java
+++ bitcoinj-server/src/main/java/org/namecoin/bitcoinj/spring/service/NameLookupService.java
@@ -9,7 +9,10 @@ import org.consensusj.namecoin.jsonrpc.rpcserver.NamecoinJsonRpc;
 
 import org.libdohj.names.NameLookupByBlockHashOneFullBlock;
 import org.libdohj.names.NameLookupByBlockHeightHashCache;
+import org.libdohj.names.NameLookupLatest;
 import org.libdohj.names.NameLookupLatestRestHeightApi;
+import org.libdohj.names.NameLookupLatestRestMerkleApi;
+import org.libdohj.names.NameLookupLatestRestMerkleApiSingleTx;
 import org.libdohj.names.NameTransactionUtils;
 import org.libdohj.script.NameScript;
 
@@ -83,7 +86,7 @@ public class NameLookupService implements NamecoinJsonRpc {
     
     protected NameLookupByBlockHashOneFullBlock lookupByHash;
     protected NameLookupByBlockHeightHashCache lookupByHeight;
-    protected NameLookupLatestRestHeightApi lookupLatest;
+    protected NameLookupLatest lookupLatest;
     
     private int timeOffset = 0;
     private BigDecimal difficulty = new BigDecimal(0);
@@ -162,7 +165,15 @@ public class NameLookupService implements NamecoinJsonRpc {
         try {
             lookupByHash = new NameLookupByBlockHashOneFullBlock(namePeerGroup);
             lookupByHeight = new NameLookupByBlockHeightHashCache(kit.chain(), lookupByHash);
-            lookupLatest = new NameLookupLatestRestHeightApi("https://namecoin.webbtc.com/name/", kit.chain(), lookupByHeight);
+            
+            // Height History API + P2P block
+            //lookupLatest = new NameLookupLatestRestHeightApi("https://namecoin.webbtc.com/name/heights/", ".json?raw", kit.chain(), lookupByHeight);
+            
+            // Merkle Branch History API
+            //lookupLatest = new NameLookupLatestRestMerkleApi(netParams, "https://namecoin.webbtc.com/name/", ".json?history&with_height&with_rawtx&with_mrkl_branch&with_tx_idx&raw", kit.chain(), kit.store(), lookupByHeight);
+            
+            // Merkle Branch Single-Transaction API (broken because the API doesn't return an array as expected)
+            lookupLatest = new NameLookupLatestRestMerkleApiSingleTx(netParams, "https://namecoin.webbtc.com/name/", ".json?with_height&with_rawtx&with_mrkl_branch&with_tx_idx&raw", kit.chain(), kit.store(), lookupByHeight);
         } catch (Exception e) {
             System.out.println("Error initializing name lookups!");
             System.out.println(e.toString());
@@ -236,21 +247,7 @@ public class NameLookupService implements NamecoinJsonRpc {
         );
     }
     
-    // TODO: document warning that this doesn't support identity isolation
-    @Override
-    public NameData name_show(String name) throws Exception {
-    
-        // TODO: add identity isolation
-        int height = lookupLatest.getHeight(name);
-        
-        return name_show_at_height(name, height);
-    }
-    
-    // TODO: document warning that this doesn't support identity isolation
-    @Override
-    public NameData name_show_at_height(String name, int height) throws Exception {
-    
-        Transaction tx = lookupByHeight.getNameTransaction(name, height, "Default identity");
+    protected NameData name_show (Transaction tx, String name) throws IOException, UnsupportedEncodingException {
         
         NameScript ns = NameTransactionUtils.getNameAnyUpdateScript(tx, name);
     
@@ -259,8 +256,29 @@ public class NameLookupService implements NamecoinJsonRpc {
             new String(ns.getOpValue().data, "ISO-8859-1"), 
             tx.getHash(), 
             ns.getAddress().getToAddress(netParams), 
-            height - getblockcount() + 36000
+            36000 - tx.getConfidence().getDepthInBlocks() + 1 // tested for correctness against webbtc API
         );
+        
+    }
+    
+    // TODO: document warning that this doesn't support identity isolation
+    @Override
+    public NameData name_show(String name) throws Exception {
+    
+        Transaction tx = lookupLatest.getNameTransaction(name, "Default identity");
+        
+        return name_show(tx, name);
+        
+    }
+    
+    // TODO: document warning that this doesn't support identity isolation
+    @Override
+    public NameData name_show_at_height(String name, int height) throws Exception {
+    
+        Transaction tx = lookupByHeight.getNameTransaction(name, height, "Default identity");
+        
+        return name_show(tx, name);
+        
     }
 
 }
