b0a9ebc282257eb4f66e021a7ed472db7fe8b1f6
JeremyRand <biolizard89@gmail.com>
2018-09-17 00:13:59 +0000

    Moved most of the initialization to NamecoinConfig so that the details can be easily customized.

--- cj-nmc-daemon/src/main/java/org/consensusj/namecoin/daemon/config/NamecoinConfig.java
+++ cj-nmc-daemon/src/main/java/org/consensusj/namecoin/daemon/config/NamecoinConfig.java
@@ -1,6 +1,23 @@
 package org.consensusj.namecoin.daemon.config;
 
 import com.fasterxml.jackson.databind.Module;
+
+import org.libdohj.names.NameLookupByBlockHash;
+import org.libdohj.names.NameLookupByBlockHashOneFullBlock;
+import org.libdohj.names.NameLookupByBlockHeight;
+import org.libdohj.names.NameLookupByBlockHeightHashCache;
+import org.libdohj.names.NameLookupLatest;
+import org.libdohj.names.NameLookupLatestRestHeightApi;
+import org.libdohj.names.NameLookupLatestRestMerkleApi;
+import org.libdohj.names.NameLookupLatestRestMerkleApiSingleTx;
+
+import org.bitcoinj.core.Context;
+import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.store.BlockStore;
+import org.bitcoinj.store.BlockStoreException;
+import org.bitcoinj.store.LevelDBBlockStore;
+
 import com.googlecode.jsonrpc4j.spring.JsonServiceExporter;
 import com.msgilligan.bitcoinj.json.conversion.RpcServerModule;
 import org.bitcoinj.core.Context;
@@ -11,8 +28,10 @@ import org.bitcoinj.kits.WalletAppKit;
 import org.consensusj.namecoin.jsonrpc.rpcserver.NamecoinJsonRpc;
 import org.libdohj.params.NamecoinMainNetParams;
 import org.namecoin.bitcoinj.spring.service.NameLookupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,12 +41,26 @@ import java.io.FileNotFoundException;
  */
 @Configuration
 public class NamecoinConfig {
+
+    @Autowired
+    private Environment env;
+
     @Bean
     public NetworkParameters networkParameters() {
         // TODO: We may also want to make this set from a configuration string
         // so a binary release can be configure via external string parameters
         // and NetworkParameters.fromID()
         return NamecoinMainNetParams.get();
+    }
+
+    private Context context;
+
+    @Bean
+    public Context getContext(NetworkParameters params) {
+        if (context == null) {
+            context = new Context(params);
+        }
+        return context;
     }
 
     /*
@@ -39,6 +72,116 @@ public class NamecoinConfig {
         return pd;
     }
     */
+
+    private WalletAppKit kit;
+
+    @Bean
+    public WalletAppKit getKit(Context context) {
+        if (kit == null) {
+
+            // TODO: check privacy settings here
+
+            // TODO: make File(".") and filePrefix configurable
+            String filePrefix = "LibdohjNameLookupDaemon";
+
+            kit = new WalletAppKit(context, new File("."), filePrefix) {
+                @Override
+                protected BlockStore provideBlockStore(File file) throws BlockStoreException {
+                    return new LevelDBBlockStore(context, file);
+                }
+
+                @Override
+                protected void onSetupCompleted() {
+                    vPeerGroup.setMinRequiredProtocolVersion(params.getProtocolVersionNum(NetworkParameters.ProtocolVersion.MINIMUM));
+                }
+            };
+
+            // TODO: call kit.setCheckpoints so that we sync faster.
+            // See the following links:
+            // https://groups.google.com/forum/?_escaped_fragment_=topic/bitcoinj/CycE9YTS7Bs#!topic/bitcoinj/CycE9YTS7Bs
+            // https://github.com/bitcoinj/bitcoinj/blob/master/tools/src/main/resources/org/bitcoinj/tools/build-checkpoints-help.txt
+            // https://github.com/namecoin/namecoin-core/blob/master/src/chainparams.cpp#L164
+
+            // When uncommented, this allows the RPC server to use an incomplete blockchain.  This is usually insecure for name lookups.
+            // TODO: uncomment this and use a different method to detect incomplete blockchains that doesn't block the RPC server from replying with error messages.
+            //kit.setBlockingStartup(false);
+
+            // Start downloading the block chain and wait until it's done.
+            kit.startAsync();
+            kit.awaitRunning();
+        }
+
+        return kit;
+    }
+
+    private boolean getStreamIsolationEnabled() {
+        Boolean val = env.getProperty("connection.streamisolation", Boolean.class, true);
+        return val;
+    }
+
+    private boolean getProxyEnabled() {
+        Boolean val = env.getProperty("connection.proxyenabled", Boolean.class, true);
+        return val;
+    }
+
+    private NameLookupByBlockHash lookupByHash;
+
+    @Bean
+    public NameLookupByBlockHash getLookupByHash(NetworkParameters netParams, WalletAppKit kit) throws Exception {
+        if (lookupByHash == null) {
+
+            String algo = env.getProperty("namelookup.byhash.algo", "p2pfullblock");
+
+            switch (algo) {
+                case "p2pfullblock":
+                    if (getStreamIsolationEnabled()) {
+                        throw new Exception("Stream isolation is not supported.  If you are okay with reducing your privacy, disable stream isolation.");
+                    }
+                    if (getProxyEnabled()) {
+                        throw new Exception("Proxying is not supported.  If you are okay with reducing your privacy, disable proxying.");
+                    }
+
+                    PeerGroup namePeerGroup = new PeerGroup(netParams, kit.chain());
+
+                    // TODO: look into allowing non-bloom, non-headers peers since we don't use filtered blocks at the moment
+                    namePeerGroup.setMinRequiredProtocolVersion(netParams.getProtocolVersionNum(NetworkParameters.ProtocolVersion.BLOOM_FILTER));
+                    namePeerGroup.addPeerDiscovery(new DnsDiscovery(netParams));
+                    namePeerGroup.startAsync();
+
+                    lookupByHash = new NameLookupByBlockHashOneFullBlock(namePeerGroup);
+
+                    break;
+                default:
+                    throw new Exception("Invalid algorithm for lookup by block hash.");
+            }
+        }
+
+        return lookupByHash;
+    }
+
+    private NameLookupByBlockHeight lookupByHeight;
+
+    // TODO: finish this logic
+    @Bean
+    public NameLookupByBlockHeight getLookupByHeight(WalletAppKit kit, NameLookupByBlockHash lookupByHash) throws Exception {
+        if (lookupByHeight == null) {
+            lookupByHeight = new NameLookupByBlockHeightHashCache(kit.chain(), lookupByHash);
+        }
+
+        return lookupByHeight;
+    }
+
+    private NameLookupLatest lookupLatest;
+
+    // TODO: finish this logic
+    @Bean
+    public NameLookupLatest getLookupLatest(WalletAppKit kit, NameLookupByBlockHeight lookupByHeight) throws Exception {
+        if (lookupLatest == null) {
+            lookupLatest = new NameLookupLatestRestMerkleApiSingleTx(networkParameters(), "https://namecoin.webbtc.com/name/", ".json?with_height&with_rawtx&with_mrkl_branch&with_tx_idx&raw", kit.chain(), kit.store(), (NameLookupByBlockHeightHashCache)lookupByHeight);
+        }
+
+        return lookupLatest;
+    }
 
     @Bean
     public Context getContext(NetworkParameters params) {
@@ -60,9 +203,8 @@ public class NamecoinConfig {
     }
 
     @Bean
-    public NameLookupService nameLookupService(NetworkParameters params/*, PeerDiscovery peerDiscovery*/) {
-        //return new PeerGroupService(params, peerDiscovery);
-        return new NameLookupService(params);
+    public NameLookupService nameLookupService(NetworkParameters params, Context context, WalletAppKit kit, NameLookupByBlockHeight lookupByHeight, NameLookupLatest lookupLatest /*, PeerDiscovery peerDiscovery*/) {
+        return new NameLookupService(params, context, kit, lookupByHeight, lookupLatest);
     }
 
     @Bean
