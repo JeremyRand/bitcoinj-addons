edc65ccff126a3bd23d941e0cc406df64a245580
JeremyRand <biolizard89@gmail.com>
2018-09-17 03:39:29 +0000

    [WIP] Namecoin lookup server support

--- cj-nmc-daemon/src/main/java/org/consensusj/namecoin/daemon/config/NamecoinConfig.java
+++ cj-nmc-daemon/src/main/java/org/consensusj/namecoin/daemon/config/NamecoinConfig.java
@@ -3,12 +3,12 @@ package org.consensusj.namecoin.daemon.config;
 import com.fasterxml.jackson.databind.Module;
 import com.googlecode.jsonrpc4j.spring.JsonServiceExporter;
 import com.msgilligan.bitcoinj.json.conversion.RpcServerModule;
-import com.msgilligan.bitcoinj.rpcserver.BitcoinJsonRpc;
-import com.msgilligan.bitcoinj.spring.service.PeerGroupService;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.net.discovery.DnsDiscovery;
 import org.bitcoinj.net.discovery.PeerDiscovery;
+import org.consensusj.namecoin.jsonrpc.rpcserver.NamecoinJsonRpc;
 import org.libdohj.params.NamecoinMainNetParams;
+import org.namecoin.bitcoinj.spring.service.NameLookupService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +27,7 @@ public class NamecoinConfig {
         return NamecoinMainNetParams.get();
     }
 
+    /*
     @Bean
     public PeerDiscovery peerDiscovery(NetworkParameters params) throws FileNotFoundException {
         PeerDiscovery pd;
@@ -34,6 +35,7 @@ public class NamecoinConfig {
 //        pd = new SeedPeers(params);
         return pd;
     }
+    */
 
     @Bean
     public Module bitcoinJMapper() {
@@ -41,15 +43,16 @@ public class NamecoinConfig {
     }
 
     @Bean
-    public PeerGroupService peerGroupService(NetworkParameters params, PeerDiscovery peerDiscovery) {
-        return new PeerGroupService(params, peerDiscovery);
+    public NameLookupService nameLookupService(NetworkParameters params/*, PeerDiscovery peerDiscovery*/) {
+        //return new PeerGroupService(params, peerDiscovery);
+        return new NameLookupService(params);
     }
 
     @Bean(name="/")
-    public JsonServiceExporter bitcoinServiceExporter(PeerGroupService peerGroupService) {
+    public JsonServiceExporter namecoinServiceExporter(NameLookupService nameLookupService) {
         JsonServiceExporter exporter = new JsonServiceExporter();
-        exporter.setService(peerGroupService);
-        exporter.setServiceInterface(BitcoinJsonRpc.class);
+        exporter.setService(nameLookupService);
+        exporter.setServiceInterface(NamecoinJsonRpc.class);
         exporter.setBackwardsComaptible(true);
         return exporter;
     }
